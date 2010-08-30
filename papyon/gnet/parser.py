9bd501f1fd7bfb39ac09d952d5fc976d6295e61f
Louis-Francis Ratté-Boulianne <louis-francis.ratte-boulianne@collabora.co.uk>
2010-08-30 12:27:46 -0400

    proxy: default HTTP content length should be 0 (not None)

--- papyon/gnet/parser.py
+++ papyon/gnet/parser.py
@@ -165,7 +165,7 @@ class HTTPParser(AbstractParser):
     def _reset_state(self):
         self._next_chunk = self.CHUNK_START_LINE
         self._receive_buffer = ""
-        self._content_length = None
+        self._content_length = 0
         self._parser.delimiter = "\r\n"
 
     def _on_received(self, transport, buf, length):

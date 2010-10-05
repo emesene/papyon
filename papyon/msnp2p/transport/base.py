751050afe516603bde86e66c4bf752a447c85316
Louis-Francis Ratté-Boulianne <louis-francis.ratte-boulianne@collabora.co.uk>
2010-10-05 18:42:06 -0400

    p2p: create a different blob queue for each session
    The signaling blobs are added to a different queue with ID = 0

--- papyon/msnp2p/transport/base.py
+++ papyon/msnp2p/transport/base.py
@@ -93,21 +93,21 @@ class BaseP2PTransport(gobject.GObject):
     def can_send(self, peer, peer_guid, blob, bootstrap=False):
         raise NotImplementedError
 
+    def has_data_to_send(self):
+        return (len(self._data_blob_queue) > 0)
+
     def send(self, peer, peer_guid, blob):
         self._queue_lock.acquire()
-        self._data_blob_queue.append((peer, peer_guid, blob))
+        queue = self._data_blob_queue.setdefault(blob.session_id, [])
+        queue.append((peer, peer_guid, blob))
         self._queue_lock.release()
         self._start_processing()
 
     def cleanup(self, session_id):
         # remove this session's blobs from the data queue
         self._queue_lock.acquire()
-        canceled_blobs = []
-        for blob in self._data_blob_queue:
-            if blob[2].session_id == session_id:
-                canceled_blobs.append(blob)
-        for blob in canceled_blobs:
-            self._data_blob_queue.remove(blob)
+        if session_id in self._data_blob_queue:
+            del self._data_blob_queue[session_id]
         self._queue_lock.release()
 
     def close(self):
@@ -124,7 +124,7 @@ class BaseP2PTransport(gobject.GObject):
     def _reset(self):
         self._queue_lock.acquire()
         self._first = True
-        self._data_blob_queue = []
+        self._data_blob_queue = {} # session_id : [(peer, peer_guid, blob)]
         self._pending_blob = {} # ack_id : (blob, callback, errback)
         self._pending_ack = set()
         self._signaling_blobs = {} # blob_id : blob
@@ -206,7 +206,7 @@ class BaseP2PTransport(gobject.GObject):
     def _process_send_queue(self):
         if not self._queue_lock.acquire(False):
             return True
-        if len(self._data_blob_queue) == 0:
+        if not self.has_data_to_send():
             self._queue_lock.release()
             self._stop_processing()
             return False
@@ -216,15 +216,23 @@ class BaseP2PTransport(gobject.GObject):
             self._stop_processing()
             return True
 
+        # FIXME find a better algorithm to choose session
+        if 0 in self._data_blob_queue:
+            session_id = 0
+        else:
+            session_id = self._data_blob_queue.keys()[0]
+
         sync = self._first
         self._first = False
-        (peer, peer_guid, blob) = self._data_blob_queue[0]
+        (peer, peer_guid, blob) = self._data_blob_queue[session_id][0]
         chunk = blob.get_chunk(self.version, self.max_chunk_size, sync)
         self.__send_chunk(peer, peer_guid, chunk)
 
         if blob.is_complete():
-            self._data_blob_queue.pop(0)
+            self._data_blob_queue[session_id].pop(0)
             self._add_pending_blob(chunk.ack_id, blob)
+        if len(self._data_blob_queue[session_id]) == 0:
+            del self._data_blob_queue[session_id]
         self._queue_lock.release()
         return True
 

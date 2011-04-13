ebf8b57cff538c2f405377b4889e7a0f3bbacd6a
Louis-Francis Ratté-Boulianne <louis-francis.ratte-boulianne@collabora.co.uk>
2011-04-12 23:40:28 -0400

    package: don't add tests files to package
    They were installed in python site-package root directory. That needs to be fixed before packaging the tests again.

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ if pieces[-1] == '':
 else:
     len_root_dir = len(pieces)
 
-for directory in ('papyon', 'tests'):
+for directory in ('papyon'):
     papyon_dir = os.path.join(root_dir, directory)
     for dirpath, dirnames, filenames in os.walk(papyon_dir):
         # Ignore dirnames that start with '.'

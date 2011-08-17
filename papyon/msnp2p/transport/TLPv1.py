181acfae9e6a7751e67d3115ecd364b315e82c45
Sven (Sbte) <svenb.linux@gmail.com>
2011-08-17 22:07:21 +0200

    bugfix: Pass strings to the logger, not Exceptions

--- papyon/msnp2p/transport/TLPv1.py
+++ papyon/msnp2p/transport/TLPv1.py
@@ -102,10 +102,10 @@ class TLPHeader(object):
             self.chunk_size = chunk_size
 
         if self.blob_offset + self.chunk_size > self.blob_size:
-            logger.warning(TLPParseError(1, "chunk end exceeds blob size", header))
+            logger.warning(str(TLPParseError(1, "chunk end exceeds blob size", header)))
             self.chunk_size = chunk_size
         if self.blob_size >= 0 and self.chunk_size == 0:
-            logger.warning(TLPParseError(1, "empty chunk for non-empty blob", header))
+            logger.warning(str(TLPParseError(1, "empty chunk for non-empty blob", header)))
             self.blob_size = 0
 
 

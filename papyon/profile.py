eb93059f9f393636eafddc278c4fd002423a6469
Riccardo (C10uD) <c10ud.dev@gmail.com>
2012-06-27 16:32:48 +0200

    papyon: import endpoint changes by @moneycat

--- papyon/profile.py
+++ papyon/profile.py
@@ -270,6 +270,9 @@ class ClientCapabilities(gobject.GObject):
             client_id = str(self.capabilities)
         return client_id
 
+    def __eq__(self, other):
+        return str(self) == str(other)
+
 
 class NetworkID(object):
     """Refers to the contact Network ID"""
@@ -392,6 +395,10 @@ class BaseContact(gobject.GObject):
                 (object,)),
 
             "end-point-removed": (gobject.SIGNAL_RUN_FIRST,
+                gobject.TYPE_NONE,
+                (object,)),
+
+            "end-point-updated": (gobject.SIGNAL_RUN_FIRST,
                 gobject.TYPE_NONE,
                 (object,)),
             }
@@ -587,15 +594,14 @@ class BaseContact(gobject.GObject):
         added_eps = set(new_eps.keys()) - set(old_eps.keys())
         removed_eps = set(old_eps.keys()) - set(new_eps.keys())
         union_eps = set(old_eps.keys()) & set(new_eps.keys())
+
         for ep in added_eps:
             self.emit("end-point-added", new_eps[ep])
         for ep in removed_eps:
             self.emit("end-point-removed", old_eps[ep])
-        # NOTE: Can we just update endpoint name?
         for ep in union_eps:
-            if new_eps[ep].name != old_eps[ep].name:
-                self.emit("end-point-removed", old_eps[ep])
-                self.emit("end-point-added", new_eps[ep])
+            if not new_eps[ep] == old_eps[ep]:
+                self.emit("end-point-updated", new_eps[ep])
 
     def do_get_property(self, pspec):
         name = pspec.name.lower().replace("-", "_")
